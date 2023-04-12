71d36dd67712417913e16675379483ad5a35883b
thakyZ <thakyz@outlook.com>
2023-04-12 16:33:04 -0700

    Updated stuff

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# SoupCatUtils
-Sample text for Soup Cat Utils!
+# Soup Cat Utils
+
+The Soup Cat made a test plugin for themselves!
+
+Please don't use this, and if you do, it's your own fault if anything happens. I won't provide binaries.
---
+++ SoupCatUtils/Utils/ImGuiExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.Intrinsics.X86;
+using System.Text;
+using System.Threading.Tasks;
+
+using ImGuiNET;
+
+namespace NekoBoiNick.FFXIV.DalamudPlugin.SoupCatUtils.Utils;
+class ImGuiExtensions {
+}
